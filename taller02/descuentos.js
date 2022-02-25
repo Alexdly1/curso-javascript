090b5e722ed8f158d8d4a5d49942f2af4aa3662b
alexander <alexanderdly1@gmail.com>
2022-02-24 21:07:19 -0500

    reto 2 terminado

--- taller02/descuentos.js
+++ taller02/descuentos.js
@@ -21,9 +21,42 @@ function onClickButtonPriceDicount() {
     resulP.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
 
-/* console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento,
-}); */
+function onClickButtonPriceDiscount1() {
+
+    const coupons = [
+        {
+            name: "Batman",
+            discount: 15,
+        },
+        {
+            name: "Superman",
+            discount: 30,
+        },
+        {
+            name: "Spiderman",
+            discount: 25,
+        },
+    ];
+
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    if (!userCoupon) {
+        alert("El cupón "+ couponValue + " no es válido");
+    }else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+        const resultM = document.getElementById("ResultP");
+        resultM.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }
+  }
